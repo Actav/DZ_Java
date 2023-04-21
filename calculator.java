351f271f0dfbba38f39187d2209201fd9b3a79da
Actav <hp@actav.com>
2023-04-21 04:42:02 +0300

    LS02 task_04

--- calculator.java
+++ calculator.java
@@ -1,7 +1,6 @@
 import com.sun.tools.javac.Main;
 
 import java.io.IOException;
-import java.util.Arrays;
 import java.util.Scanner;
 import java.util.logging.FileHandler;
 import java.util.logging.Level;
