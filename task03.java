90f04116550b43283c498cfe45a88755f2b4ac8e
Actav <hp@actav.com>
2023-04-21 04:06:55 +0300

    LS02 task_03

--- task03.java
+++ task03.java
@@ -3,7 +3,7 @@ import java.util.regex.Pattern;
 
 public class task03 {
     public static void main(String[] args) {
-        final String regex = "\"fio\":\"(.*?)\\\",\"grade\":\"(.)\",\"subject\":\"(.*?)\"";
+        final String regex = "\"fio\":\"(.*?)\",\"grade\":\"(.)\",\"subject\":\"(.*?)\"";
         final String string = "[{\"fio\":\"Ivanov\",\"grade\":\"5\",\"subject\":\"Mathematics\"},{\"fio\":\"Petrov\",\"grade\":\"4\",\"subject\":\"Informatics\"},{\"fio\":\"Krasnov\",\"grade\":\"5\",\"subject\":\"Physics\"}]";
 
         final Pattern pattern = Pattern.compile(regex, Pattern.MULTILINE);
