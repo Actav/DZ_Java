d27ac378d29539846f2a6a2fb70ddb62d6cfe8f4
Actav <hp@actav.com>
2023-04-29 00:01:42 +0300

    DZ LS_04 task_01

---
+++ reversed.java
@@ -0,0 +1,20 @@
+import java.util.Arrays;
+import java.util.LinkedList;
+
+public class reversed {
+    public static void main(String[] args) {
+        LinkedList<Integer> list = new LinkedList<>(Arrays.asList(5, 4, 3, 2, 1));
+
+        LinkedList<Integer> reversed = reverseList(list);
+        System.out.println(reversed);
+    }
+
+    public static LinkedList<Integer> reverseList(LinkedList<Integer> list) {
+        LinkedList<Integer> reversed = new LinkedList<Integer>();
+        for (Integer item : list) {
+            reversed.addFirst(item);
+        }
+
+        return reversed;
+    }
+}
