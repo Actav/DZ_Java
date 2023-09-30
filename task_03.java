82db361d16110477d23aef708b3edaee27a29c9c
Actav <hp@actav.com>
2023-09-30 15:47:58 +0300

    DZ_02

--- task_03.java
+++ task_03.java
@@ -30,12 +30,12 @@ public class task_03 {
             printSum(23, 234);
 
             int[] abc = {1, 2};
-            // abc[3] = 9; // Вызывает ArrayIndexOutOfBoundsException
+            abc[3] = 9; // Вызывает ArrayIndexOutOfBoundsException
         } catch (NullPointerException ex) {
             System.out.println("Указатель не может указывать на null!");
         } catch (IndexOutOfBoundsException ex) {
             System.out.println("Массив выходит за пределы своего размера!");
-        } catch (Throwable ex) {
+        } catch (Throwable ex) { // Throwable является базовым классом для всех исключений, и его обработка должна идти последней
             System.out.println("Что-то пошло не так...");
         }
     }
